94a70e3f3f21a6997892375baf9d2f22d6c582d4
splatte <sp@x-berg.de>
2015-01-30 19:43:57 +0100

    fix xml parsing, basic support for new mipmap app icons

--- src/main/groovy/com/github/splatte/android/OverlayTask.groovy
+++ src/main/groovy/com/github/splatte/android/OverlayTask.groovy
@@ -18,11 +18,12 @@ class OverlayTask extends DefaultTask {
          * parse AndroidManifest.xml
          * find file name for app icon in <application .. android:icon="@drawable/ic_launcher">
          */
-        def manifestXml = new XmlSlurper().parse(manifestFile)
-        def iconFileName = manifestXml.application.@icon.text().split("/")[1]
+        def manifestXml = new XmlSlurper().parse(manifestFile).declareNamespace('android':'http://schemas.android.com/apk/res/android')
+        def iconFileName = manifestXml.application.@'android:icon'.text().split("/")[1]
 
         /* find the app icon files in all 'drawable' folders */
-        resourcesPath.eachDirMatch(~/^drawable.*/) { dir ->
+        // TODO: use ..split("/")[0] to determine if drawable or mipmap
+        resourcesPath.eachDirMatch(~/^drawable.*|^mipmap.*/) { dir ->
             dir.eachFileMatch(FileType.FILES, ~"^${iconFileName}.*") { file ->
                 logger.debug("found file: ${file}")
 
@@ -31,6 +32,7 @@ class OverlayTask extends DefaultTask {
                 def caption = new SimpleTemplateEngine().createTemplate(project.appiconoverlay.format).make(formatBinding)
 
                 /* invoke ImageMagick */
+                // TODO: when there is no .git, `caption` will be empty and the imagemagick call will never complete
                 def imagemagick = ["${project.appiconoverlay.imageMagick}",
                     "-background", "${project.appiconoverlay.backgroundColor}",
                     "-fill", "${project.appiconoverlay.textColor}",
